47f80c66a712d9d4b5f73727d20b33aa3997360f
Milena Pereira <milena.psilva03@gmail.com>
2025-03-08 09:10:36 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <header class="banner">
+    <div class="apresentacao">
+      <img
+        src="./assets/imagens/logo.svg"
+        alt="logo do Cookin' Up"
+        class="logo"
+      />
+      <p class="cabecalho-lh frase-cabecalho"></p>
+    </div>
+  </header>
+</template>
