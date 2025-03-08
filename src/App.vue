5e36e087c49c682a1fe0bc5a1acd7dbbd4cba5c5
Milena Pereira <milena.psilva03@gmail.com>
2025-03-08 12:49:43 -0300

    feat: create Banner component for page header

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,10 @@
+<script>
+import Banner from "./components/Banner.vue";
+
+export default {
+  components: { Banner },
+};
+</script>
 <template>
-  <header class="banner">
-    <div class="apresentacao">
-      <img
-        src="./assets/imagens/logo.svg"
-        alt="logo do Cookin' Up"
-        class="logo"
-      />
-      <p class="cabecalho-lh frase-cabecalho"></p>
-    </div>
-  </header>
+  <Banner />
 </template>
